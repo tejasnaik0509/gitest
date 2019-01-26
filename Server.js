568cb443771b2d63e164cf31160b428f308bdf9e
tejasnaik0509 <naiktejas0509@gmail.com>
2019-01-26 14:44:02 +0530

    fourth commit

--- Server.js
+++ Server.js
@@ -1,5 +1,5 @@
 //creating web server using http module
-var http = require('http');
+var http = require('http'); //for "https" use https instead of http. 
 
 http.createServer(function(req, res){
 
@@ -8,3 +8,14 @@ http.createServer(function(req, res){
     res.end();
 
 }).listen(3000);
+
+
+//ES6 code for http connection
+/* http.createServer((req, res) => {
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("Request receive at Server" + req.url+ "<br />");
+    res.write("Hello from server 1");
+    res.end();
+
+}).listen(3000); */
